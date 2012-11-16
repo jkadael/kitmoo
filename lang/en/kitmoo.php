847c61de9c485c75b1f1d54894f85d19a200fce1
Jair Riano <jkadael@gmail.com>
2012-11-16 12:28:55 -0500

    Lenguaje ingles modulo

---
+++ lang/en/kitmoo.php
@@ -0,0 +1,87 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Cadenas del idioma inglés que utiliza el Módulo de Actividad Kitmoo
+ *
+ * @package    mod
+ * @subpackage kitmoo
+ * @copyright  2012 Jair Riaño, Andrés Rodríguez
+ * @copyright  2012 DHumATIC - Universidad de los Llanos <http://www.unillanos.edu.co/>
+ * @author     Jair Riaño <jkadael@gmail.com>
+ * @author     Andrés Rodríguez <kiddavid180@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Evitar ejecución directa que revelaría mensajes de error en servidores mal configurados.
+defined('MOODLE_INTERNAL') || die();
+
+// Usadas por Moodle - Generales - Obligatorias.
+$string['modulename'] = 'Kitmoo';
+$string['modulenameplural'] = 'Kitmoos';
+$string['modulename_help'] = 'Kitmoo is a module for creating graphical environments and, thus, be more appealing to students.';
+$string['pluginadministration'] = 'Kitmoo Administration';
+$string['pluginname'] = 'Kitmoo';
+$string['kitmoo'] = 'Kitmoo';
+$string['page-mod-kitmoo-x'] = 'Any Kitmoo module page';
+
+// Archivo index.php.
+$string['nokitmoos'] = 'There are no instances of Kitmoo.';
+
+// Capacidades del módulo.
+$string['kitmoo:create'] = 'Create Kitmoo';
+$string['kitmoo:view'] = 'View Kitmoo';
+
+// Archivo mod_form.php.
+$string['kitmoointro'] = 'Kitmoo Description';
+$string['kitmooname'] = 'Kitmoo Name';
+
+// Archivo create.php.
+$string['title'] = 'Kitmoo Toolkit';
+$string['version'] = 'Version 1.0';
+$string['tools'] = 'Tools';
+$string['editpage'] = 'Edit Page';
+$string['message'] = 'Select the icon to use and drag to create the activity or resource selected.';
+$string['resources'] = 'Resources';
+$string['file'] = 'File';
+$string['descriptionfile'] = 'An image, a PDF document, a spreadsheet, a sound file, a video file.';
+$string['url'] = 'URL';
+$string['descriptionurl'] = 'You can send students anywhere via browser. Flickr, Youtube, Wikipedia.';
+$string['page'] = 'Page';
+$string['descriptionpage'] = 'The student sees a navigable and simple page that the teacher creates a robust HTML editor.';
+$string['activities'] = 'Activities';
+$string['forum'] = 'Forum';
+$string['descriptionforum'] = 'Activity for discussion between users of a course.';
+$string['chat'] = 'Chat';
+$string['descriptionchat'] = 'Enables conversations between users in real time.';
+$string['scorm'] = 'Scorm';
+$string['descriptionscorm'] = 'Bundle of Web content packaged following the SCORM learning objects.';
+$string['wiki'] = 'Wiki';
+$string['descriptionwiki'] = 'Enables documents to be authored collectively in a simple markup language using a Web Browser.';
+$string['theme'] = 'Theme Chooser';
+$string['themeone'] = 'Technology Topics';
+$string['themetwo'] = 'Agronomic Topics';
+$string['themethree'] = 'Economic Topics';
+$string['themefour'] = 'Veterinary Topics';
+$string['ready'] = 'Ready';
+$string['alertone'] = 'Make sure this activity or resource?';
+$string['alerttwo'] = 'Sure to remove the selected icon?';
+$string['language'] = 'en';
+
+// Otros.
+$string['info'] = 'Here are the proposed activities. Scroll the image and find icons in different tasks.';
+$string['buttoncreate'] = 'Configure your Graphical Environment';
+$string['advice'] = 'Click to start the activity';
